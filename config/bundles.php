7490053f6c2aaa3a7a942334ac56a1985f9caf66
Gabriel Alcántara <gabrielalcantara.93@gmail.com>
2018-05-25 12:24:36 +0200

    Enlace con BBDD
    Crear la base de datos (Configuración en el fichero .env)
    php bin/console doctrine:database:create

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,8 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
